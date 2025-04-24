88c4a66f97a655db9b0ff1032406aa321dfe9912
Javier.rc <javier.rodriguez.external@eviden.com>
2025-04-24 11:53:52 +0200

    primeras pruebas - 24.04.25_11.53

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'miprimeraApp';
+  saludo = 'Hola amigos de Angular. Como va todo?'
 }
