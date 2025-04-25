b6ce2d4ad77b52375183b765df4421fa9d0a47f4
Javier.rc <javier.rodriguez.external@eviden.com>
2025-04-25 13:12:15 +0200

    cambios - 25.04.25_13.12

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,13 +11,33 @@ import { Component } from '@angular/core';
   //styles: ["p{background-color: red;}"]
 })
 export class EmpleadoComponent {
-  nombre = 'Juan';
+  nombre = 'Javi';
   apellido = 'Diaz';
   edad = 28;
-  empresa = 'ATOS';
+  //empresa = 'ATOS';
+  habilitacionCuadro = false;
+  usuRegistrado = false;
+  textoDeRegistro = 'No hay nadie registrado';
 
   /* getEdad(){
     return this.edad;
   } */
+
+  getRegistroUsuario(){
+    this.usuRegistrado = false;
+  }
+  setUsuarioRegistrado(event: Event){
+    //alert ("El usuario se acaba de registrar")
+    //this.textoDeRegistro = 'El usuario se acaba de registrar';
+    if((<HTMLInputElement>event.target).value == 'si') {
+      this.textoDeRegistro = 'El usuario se acaba de registrar';
+    } else{
+      this.textoDeRegistro = 'No hay nadie registrado';
+    }
+  }
+
+  llamaEmpresa(value: String){}
+
+
 
 }
